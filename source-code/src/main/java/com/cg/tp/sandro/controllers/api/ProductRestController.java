5fad98e76c95d831c4abec4b70585a179e282d76
HoangDucPhuong0404 <phuongduc826@gmail.com>
2022-09-16 15:25:01 +0700

    refactor resource

--- source-code/src/main/java/com/cg/tp/sandro/controllers/api/ProductRestController.java
+++ source-code/src/main/java/com/cg/tp/sandro/controllers/api/ProductRestController.java
@@ -77,7 +77,6 @@ public class ProductRestController {
             return appUtils.mapErrorToResponse(bindingResult);
         }
 
-
         ProductResult newProduct = productService.create(param);
         return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
     }
